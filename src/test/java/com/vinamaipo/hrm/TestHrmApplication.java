0e047201893f64b548c5a547ad1ba68edfcbd95d
JonatanWang <jonatan.wang@gmail.com>
2021-03-17 09:39:12 +0100

    Add testing for AuthApi

---
+++ src/test/java/com/vinamaipo/hrm/TestHrmApplication.java
@@ -0,0 +1,13 @@
+package com.vinamaipo.hrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HrmApplication {
+
+    @Test
+    public void contextLoads() {
+    }
+}
+
